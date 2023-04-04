8362320312d83bcde9bb1cd12a19dd6a42cdd997
Nelson Baez <nelbaeesc@alu.edu.gva.es>
2023-04-04 21:11:05 +0200

    clase

--- app/src/main/java/com/example/myweatherbase/activities/MainActivity.java
+++ app/src/main/java/com/example/myweatherbase/activities/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends BaseActivity implements CallInterface {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //a
 
         recyclerView = findViewById(R.id.recyclerView);
 
